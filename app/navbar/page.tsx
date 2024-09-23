b4565a040c90c6abd46f9e4dccf976991a0debdb
Akshay A <akshay6.nitt@gmail.com>
2024-09-23 22:43:07 +0530

    feat(startup): Mediva for the win

---
+++ app/navbar/page.tsx
@@ -0,0 +1,5 @@
+export default function Navbar() {
+  return (
+    <div className='flex h-[70px] w-screen bg-white px-[15%] items-center justify-center font-montserrat border-b-[1px] border-b-[#00000098]'></div>
+  );
+}
