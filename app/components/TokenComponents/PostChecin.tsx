49d0e23f7cdb600559ae330afa32fbef5e960ac3
Vignesh Duraisamy <vigneshd332@gmail.com>
2024-12-18 01:49:07 +0530

    fix:live token page

--- app/components/TokenComponents/PostChecin.tsx
+++ app/components/TokenComponents/PostChecin.tsx
@@ -1,20 +1,28 @@
-export default function PostCheckin() {
+export default function PostCheckin({
+  position,
+  tokenNumber,
+  tokenTime,
+}: {
+  position: number;
+  tokenNumber: number;
+  tokenTime: string;
+}) {
   return (
-    <div className='h-[300px] w-[80%] md:w-[60%] lg:w-[40%] border-[3px] border-solid border-black rounded-xl self-center m-[50px] flex flex-col items-center justify-center'>
-      <div className='h-[90%] w-full flex flex-col items-center justify-center'>
-        {' '}
-        <p className='font-semibold'>Token no.</p>
-        <h1 className='text-[2.4rem] sm:text-[3rem] font-bold text-[#000000] '>
-          17
+    <div className="h-[300px] w-[80%] md:w-[60%] lg:w-[40%] border-[3px] border-solid border-black rounded-xl self-center m-[50px] flex flex-col items-center justify-center">
+      <div className="h-[90%] w-full flex flex-col items-center justify-center">
+        {" "}
+        <p className="font-semibold">Token no.</p>
+        <h1 className="text-[2.4rem] sm:text-[3rem] font-bold text-[#000000] ">
+          {tokenNumber}
         </h1>
-        <p className='font-semibold'>Alloted Time</p>
-        <h1 className='text-[2.4rem] sm:text-[3rem] font-semibold text-[#2FA58E] '>
-          6 : 30 pm
+        <p className="font-semibold">Alloted Time</p>
+        <h1 className="text-[2.4rem] sm:text-[3rem] font-semibold text-[#2FA58E] ">
+          {tokenTime}
         </h1>
       </div>
-      <div className='h-[10%] w-full bg-black flex flex-row items-center justify-center text-white text-[0.9rem]'>
-        <span className='font-bold text-[1rem] mx-[5px]'>7</span> patients in
-        front of you
+      <div className="h-[10%] w-full bg-black flex flex-row items-center justify-center text-white text-[0.9rem]">
+        <span className="font-bold text-[1rem] mx-[5px]">{position}</span>{" "}
+        patient(s) in front of you
       </div>
     </div>
   );
--- app/livetoken/page.tsx
+++ app/livetoken/page.tsx
@@ -1,39 +1,57 @@
-'use client';
-import { ChangeEvent, useEffect, useState } from 'react';
-import Image from 'next/image';
-import logo from './../assets/logo-black.png';
-import optionsIcon from './../assets/options.svg';
-import arrow from './../assets/arrow_status.svg';
-import mapsIcon from './../assets/maps_icon.png';
-import successfull from './../assets/successfull.svg';
-import { Combobox, useCombobox, Switch } from '@mantine/core';
-import Link from 'next/link';
-import PostCheckin from '../components/TokenComponents/PostChecin';
-import Precheckin from '../components/TokenComponents/PreCheckin';
+"use client";
+import { ChangeEvent, useEffect, useState } from "react";
+import Image from "next/image";
+import logo from "./../assets/logo-black.png";
+import optionsIcon from "./../assets/options.svg";
+import arrow from "./../assets/arrow_status.svg";
+import mapsIcon from "./../assets/maps_icon.png";
+import successfull from "./../assets/successfull.svg";
+import { Combobox, useCombobox, Switch } from "@mantine/core";
+import Link from "next/link";
+import PostCheckin from "../components/TokenComponents/PostChecin";
+import Precheckin from "../components/TokenComponents/PreCheckin";
+import { API_URL } from "../config/config";
 
 export default function Livetoken() {
   const [checkin, setCheckin] = useState(false);
-  const [name, setName] = useState('Vignesh');
-  const [doctor, setDoctor] = useState('Dr. Mukesh');
-  const [checked, setChecked] = useState(false);
+  const [patientId, setPatientId] = useState<string | null>(null);
+  const [position, setPosition] = useState(0);
+  const [tokenNumber, setTokenNumber] = useState(0);
+  const [tokenTime, setTokenTime] = useState("");
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
-    if (searchParams.get('checkin') === 'true') {
-      setCheckin(true);
-    }
-    if (searchParams.get('name') !== null) {
-      setName(searchParams.get('name') as string);
-    }
-    if (searchParams.get('doctor') !== null) {
-      setDoctor(searchParams.get('doctor') as string);
+    if (searchParams.get("patientId")) {
+      setPatientId(searchParams.get("patientId"));
     }
   }, [checkin]);
 
+  useEffect(() => {
+    if (!patientId) return;
+    const fetchData = async () => {
+      const response = await fetch(
+        `${API_URL}/patient/getTokenStatus/${patientId}`
+      );
+      if (!response.ok) {
+        console.log("Error fetching data");
+        return;
+      }
+      const data = await response.json();
+      setPosition(data.position);
+      setTokenNumber(data.tokenNumber);
+      setTokenTime(data.currentTime);
+    };
+    const interval = setInterval(fetchData, 5000);
+    fetchData();
+    return () => {
+      clearInterval(interval);
+    };
+  }, [patientId]);
+
   const [location] = useState(
-    'https://www.google.com/maps/dir/13.0912037,80.2180086/Cogent+Care+(+ENT+n+Gynae+Clinic),+Whitefield+Main+Rd,+opposite+4+Point+Sheraton+Hotel,+Above+Caf%C3%A9+Coffee+Day,+Happy+Valley,+Whitefield,+Bengaluru,+Karnataka+560066/@12.8510397,76.3353326,7z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x3bae0e06b5824ec7:0x2f7051c7fb29e0c6!2m2!1d77.751292!2d12.979305!3e0?entry=ttu&g_ep=EgoyMDI0MTIwNC4wIKXMDSoASAFQAw%3D%3D'
+    "https://www.google.com/maps/dir/13.0912037,80.2180086/Cogent+Care+(+ENT+n+Gynae+Clinic),+Whitefield+Main+Rd,+opposite+4+Point+Sheraton+Hotel,+Above+Caf%C3%A9+Coffee+Day,+Happy+Valley,+Whitefield,+Bengaluru,+Karnataka+560066/@12.8510397,76.3353326,7z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x3bae0e06b5824ec7:0x2f7051c7fb29e0c6!2m2!1d77.751292!2d12.979305!3e0?entry=ttu&g_ep=EgoyMDI0MTIwNC4wIKXMDSoASAFQAw%3D%3D"
   );
-  const groceries = ['Contact hospital', `Doctor's profile`, 'Cancel Bookings'];
+  const groceries = ["Contact hospital", `Doctor's profile`, "Cancel Bookings"];
   // const [selectedItem, setSelectedItem] = useState<string | null>(null);
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
@@ -44,46 +62,33 @@ export default function Livetoken() {
       {item}
     </Combobox.Option>
   ));
-  const handleCheckin = (event: ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-    if (event.target.checked) {
-      setCheckin(true);
-      const url = new URL(window.location.href);
-      url.searchParams.set('checkin', 'true');
-      window.history.pushState({}, '', url.toString());
-    } else {
-      const url = new URL(window.location.href);
-      url.searchParams.set('checkin', 'false');
-      window.history.pushState({}, '', url.toString());
-      setCheckin(false);
-    }
-  };
+
   return (
-    <div className='text-black flex h-screen w-screen bg-background px-[4%] md:px-[10%]  font-montserrat flex-col items-center  relative overflow-y-hidden'>
-      <Link href={location} className=' w-full fixed  bottom-0'>
-        {' '}
-        <div className=' w-full h-[50px] rounded-tr-2xl rounded-tl-2xl bg-black flex flex-row items-center justify-around p-[5px] px-[10px]'>
-          <Image src={mapsIcon} alt='india' className='h-[20px] w-auto'></Image>
-          <p className='text-white text-[0.8rem] ml-[10px]'>
+    <div className="text-black flex h-screen w-screen bg-background px-[4%] md:px-[10%]  font-montserrat flex-col items-center  relative overflow-y-hidden">
+      <Link href={location} className=" w-full fixed  bottom-0">
+        {" "}
+        <div className=" w-full h-[50px] rounded-tr-2xl rounded-tl-2xl bg-black flex flex-row items-center justify-around p-[5px] px-[10px]">
+          <Image src={mapsIcon} alt="india" className="h-[20px] w-auto"></Image>
+          <p className="text-white text-[0.8rem] ml-[10px]">
             Navigate to Cogent Care
           </p>
-          <Image src={arrow} alt='india' className='h-[20px] w-auto'></Image>
+          <Image src={arrow} alt="india" className="h-[20px] w-auto"></Image>
         </div>
       </Link>
 
-      <div className='h-[60px] sm:h-[70px] w-full  flex flex-row justify-between items-center mb-[50px]'>
-        <div className='flex flex-row items-center gap-[10px]'>
-          <div className='h-[15px] w-[15px] rounded-[50%] bg-[#c54242]'></div>
-          <p className='font-semibold text-[1.4rem]'>Live Token Status</p>
+      <div className="h-[60px] sm:h-[70px] w-full  flex flex-row justify-between items-center mb-[50px]">
+        <div className="flex flex-row items-center gap-[10px]">
+          <div className="h-[15px] w-[15px] rounded-[50%] bg-[#c54242]"></div>
+          <p className="font-semibold text-[1.4rem]">Live Token Status</p>
         </div>
         <Image
           src={logo}
-          alt='mediva'
-          className='h-[30px] sm:h-[50px] w-auto'
+          alt="mediva"
+          className="h-[30px] sm:h-[50px] w-auto"
         ></Image>
       </div>
-      <div className='h-auto w-full flex flex-col'>
-        <p className='text-[1.4rem] mb-[10px]'>
+      <div className="h-auto w-full flex flex-col">
+        {/* <p className='text-[1.4rem] mb-[10px]'>
           Dear <span className='font-semibold'>{name}</span>
         </p>
         <div className='flex flex-row items-center gap-[10px]'>
@@ -94,20 +99,20 @@ export default function Livetoken() {
           ></Image>
 
           <p>Your appointment with {doctor} is confirmed</p>
-        </div>
+        </div> */}
 
-        {checkin ? <PostCheckin /> : <Precheckin />}
+        {/* {checkin ? <PostCheckin /> : <Precheckin />} */}
+        <PostCheckin
+          position={position}
+          tokenNumber={tokenNumber}
+          tokenTime={tokenTime}
+        />
 
-        <div className='flex justify-between items-center'>
-          <Switch
-            checked={checked}
-            label='Checkin'
-            onChange={(event) => handleCheckin(event)}
-          />
+        <div className="flex justify-between items-center">
           <Combobox
             store={combobox}
             width={150}
-            position='bottom-start'
+            position="bottom-start"
             withArrow
             onOptionSubmit={() => {
               // setSelectedItem(val);
@@ -116,14 +121,14 @@ export default function Livetoken() {
           >
             <Combobox.Target>
               <div
-                className='cursor-pointer select-none flex flex-row items-center text-1.2rem font-medium gap-[10px]'
+                className="cursor-pointer select-none flex flex-row items-center text-1.2rem font-medium gap-[10px]"
                 onClick={() => combobox.toggleDropdown()}
               >
                 <p>Options</p>
                 <Image
                   src={optionsIcon}
-                  alt='options'
-                  className='h-[20px] w-auto'
+                  alt="options"
+                  className="h-[20px] w-auto"
                 ></Image>
               </div>
             </Combobox.Target>
